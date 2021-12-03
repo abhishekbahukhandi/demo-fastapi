fbf1d89de011f6c20ae2ddc4f0e8b7e42e3b4e60
abhishekbahukhandi <abhishekbahukhandi@gmail.com>
2021-12-04 00:15:10 +0530

    migration files fixed

--- alembic/versions/5437dd5d1e12_delete_posts_table.py
+++ alembic/versions/5437dd5d1e12_delete_posts_table.py
@@ -17,10 +17,16 @@ depends_on = None
 
 
 def upgrade():
+    op.drop_table("votes")
     op.drop_table("posts")
-    pass
 
 
 def downgrade():
     op.create_table("posts", sa.Column("id", sa.Integer(), primary_key=True, nullable=False))
-    pass
+    op.op.create_table('votes',
+    sa.Column('post_id', sa.INTEGER(), nullable=False),
+    sa.Column('user_id', sa.INTEGER(), nullable=False),
+    sa.ForeignKeyConstraint(['post_id'], ['posts.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+    sa.PrimaryKeyConstraint('post_id', 'user_id')
+    )
